dbd022607fe04b3782b84a00f2a666968cf659a2
Amine Ben Mansour <amin.bemansour.10@gmail.com>
2022-07-07 10:40:25 +0100

    setting dependencies and plugins

---
+++ dapp/contracts/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+contract Raffle {
+
+    constructor() {
+        
+    }
+
+    function pickRandomWinner() public returns(address) {
+        
+    }
+}
