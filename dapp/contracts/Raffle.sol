dd74ede24602b0589ebf5abd6f2da526d82ee6bd
Amine Ben Mansour <amin.bemansour.10@gmail.com>
2022-07-07 11:12:19 +0100

    implementing entering raffle

--- dapp/contracts/Raffle.sol
+++ dapp/contracts/Raffle.sol
@@ -1,13 +1,35 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.4;
 
+error Raffle__NotEnoughEthEntered();
+
 contract Raffle {
 
-    constructor() {
-        
+    uint256 private immutable entranceFee;
+    address payable[] private players;
+
+    constructor(uint256 _entranceFee) {
+        entranceFee = _entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+
+        players.push(payable(msg.sender));
     }
 
     function pickRandomWinner() public returns(address) {
         
     }
+
+    function getEntranceFee() public view returns (uint256) {
+        return entranceFee;
+    }
+
+    function getPlayer(uint256 _index) public view returns (address) {
+        return players[_index];
+    }
+    
 }
