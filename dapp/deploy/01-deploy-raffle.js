8cc600f31099e2f61dbda46c5488273fc251fdff
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-11 04:33:05 +0100

    getting VRF's address fr testnets

--- dapp/deploy/01-deploy-raffle.js
+++ dapp/deploy/01-deploy-raffle.js
@@ -1,14 +1,17 @@
 const { network, ethers } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
  module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
     let vrfCoordinatorV2Address
     
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    } else {
+        vrfCoordinatorV2Address = networkConfig.chainId.vrfCoordinatorV2
     }
 
     const Raffle = await deploy("Raffle", {
