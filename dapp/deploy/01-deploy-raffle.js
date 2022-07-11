a77594d4a4530bc03e1ab0cbdea7608fb7f7c4c1
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-11 11:41:54 +0100

    define subscriptionId for constructor AND funding mock VRF

--- dapp/deploy/01-deploy-raffle.js
+++ dapp/deploy/01-deploy-raffle.js
@@ -1,17 +1,30 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
- module.exports = async ({ getNamedAccounts, deployments }) => {
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
     
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        
+        const transactionResponse = await vrfCoordinatorV2Mock.creaeSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
+        
+        // Link token on a real netwok before funding the subscription
+
     }
 
     const entranceFee = networkConfig.chainId.entranceFee
