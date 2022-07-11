2113ece7951ed022ca16ead3070a8081b7e5ae5a
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-11 04:28:10 +0100

    getting mock's address

--- dapp/deploy/01-deploy-raffle.js
+++ dapp/deploy/01-deploy-raffle.js
@@ -1,8 +1,15 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
  module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    let vrfCoordinatorV2Address
+    
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    }
 
     const Raffle = await deploy("Raffle", {
         from: deployer,
