61d176501e10b13120b0051f80fca5c5557d6ee8
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-11 12:29:27 +0100

    verifying on etherscan

--- dapp/deploy/01-deploy-raffle.js
+++ dapp/deploy/01-deploy-raffle.js
@@ -1,5 +1,6 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
@@ -46,4 +47,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying ...")
+        await verify(Raffle.address, args)
+    }
+
+    log("-------------------------------------")
  }
