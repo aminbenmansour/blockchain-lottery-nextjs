2da120c2480665b3e6e9f0314c60afa4a2d9e47a
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-16 18:39:11 +0100

    add another beforeEach for fulfillRandmWords

--- dapp/test/unit/Raffle.test.js
+++ dapp/test/unit/Raffle.test.js
@@ -124,5 +124,15 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                     assert(raffleState == 1) // 0 = open, 1 = calculating
                 })
             })
+           
+        })
+
+        describe("fulfillRandomWords", async () => {
+            beforeEach(async () => {
+                await raffle.enterRaffle({ value: raffleEntranceFee })
+                await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                await network.provider.request({ method: "evm_mine", params: [] })
+              })
+            })
         })
     })
