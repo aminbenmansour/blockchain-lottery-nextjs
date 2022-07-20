9eba145d2008dd6f342f21098c0c9fe6bcb6f9e9
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-20 09:42:50 +0100

    basic connect button with hooks

--- client/components/ManualHeader.jsx
+++ client/components/ManualHeader.jsx
@@ -1,13 +1,28 @@
 import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
 const ManualHeader = () => {
 
-    const { enableWeb3, account } = useMoralis()
+    const { enableWeb3, account, isWeb3Enabled } = useMoralis()
+
+    useEffect(() => {
+        if(isWeb3Enabled) return
+        // enableWeb3()
+ 
+    }, [isWeb3Enabled])
+
     return (
     <div>
-        { account
-        ? (<div>Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}</div>)
-        : (<button onClick={async () => {(await enableWeb3())} }>Connect</button>)}
+        { account ? (
+            <div>Connected to {account.slice(0, 6)}...{account.slice(account.length - 4)}</div>
+        ) : (
+        <button 
+            onClick={async () => {
+                await enableWeb3()
+            }}
+        > Connect </button>
+        
+        )}
         
     </div>)
 }
