33fad58647bd2cfebf1c1d2be1f30b8153895d5f
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-21 11:31:29 +0100

    getting chain id from moralis and wallet

--- client/components/LotteryEntrance.jsx
+++ client/components/LotteryEntrance.jsx
@@ -1,10 +1,12 @@
-import { useWeb3Contract } from "react-moralis";
+import { useMoralis, useWeb3Contract } from "react-moralis";
 import { abi, contractAddress } from "../constants"
 
 const LotteryEntrance = () => {
 
-    // const { runContractFunction: enterRaffle } =
-    // useWeb3Contract({
+    const { chainId: chainIdHex } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
     //   abi: usdcEthPoolAbi,
     //   contractAddress: usdcEthPoolAddress,
     //   functionName: "enterRaffle",
