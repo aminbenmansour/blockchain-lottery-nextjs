5974729bc533b05cb1f0ab181828b68e79e5a652
aminbenmansour <amin.benmansour.10@gmail.com>
2022-07-21 11:15:25 +0100

    updating frontend, allworking

--- dapp/deploy/99-update-front-end.js
+++ dapp/deploy/99-update-front-end.js
@@ -1,41 +1,38 @@
 const { ethers, network } = require("hardhat")
-const { fs } = require("fs")
+const fs = require("fs")
 
-const FRONT_END_ADDRESSES_FILE = "../../client/constants/contractAddresses.json"
-const FRONT_END_ABI_FILE = "../../client/constants/abi.json"
+const FRONT_END_ADDRESSES_FILE = "../client/constants/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../client/constants/abi.json"
 
 module.exports = async () => {
     if (process.env.UPDATE_FRONT_END) {
         console.log("Updating front end")
         updateContractAddresses()
         updateAbi()
+        console.log("Front end written!")
     }
 }
 
 async function updateAbi() {
-<<<<<<< HEAD
     const raffle = await ethers.getContract("Raffle")
-=======
->>>>>>> 992907115d53e4e99a419079598938bde965ebe8
-    fs.writeFileSync(FRONT_END_ABI_FILE, ethers.interface.format(ethers.utils.FormatTypes.json))
+    fs.writeFileSync(FRONT_END_ABI_FILE, raffle.interface.format(ethers.utils.FormatTypes.json))
 }
 
 async function updateContractAddresses() {
     const raffle = await ethers.getContract("Raffle")
     const chainId = network.config.chainId.toString()
 
-    const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
+    const contractAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
 
-    if (chainId in contractAddress) {
-        if (!currentAddresses[chainId].includes(raffle.address)) {
-            currentAddresses[chainId].push(raffle.address)
+    if (chainId in contractAddresses) {
+        if (!contractAddresses[chainId].includes(raffle.address)) {
+            contractAddresses[chainId].push(raffle.address)
         }
-    }
-    {
-        currentAddresses[chainId] = [raffle.address]
+    } else {
+        contractAddresses[chainId] = [raffle.address]
     }
 
-    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(contractAddresses))
 }
 
 module.exports.tags = ["all", "frontend"]
